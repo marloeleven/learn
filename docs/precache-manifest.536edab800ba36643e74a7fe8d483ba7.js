efc2de69ca490692e62720bcc614cf5802863f6b
Marlo Dela Torre <marlodelatorre11@gmail.com>
2020-11-11 06:17:30 +0800

    initial build

---
+++ docs/precache-manifest.536edab800ba36643e74a7fe8d483ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e028af259a9200b2155dc7c89b5c847c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "034a0b1bd78eb8bde247",
+    "url": "./static/css/main.c29838fd.chunk.css"
+  },
+  {
+    "revision": "b70229cefcf7f76318ef",
+    "url": "./static/js/2.4696316c.chunk.js"
+  },
+  {
+    "revision": "45cbcc29c20783d5406c2d44ea879624",
+    "url": "./static/js/2.4696316c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034a0b1bd78eb8bde247",
+    "url": "./static/js/main.bfa449be.chunk.js"
+  },
+  {
+    "revision": "191293f5ba5c7cf4fc68",
+    "url": "./static/js/runtime-main.54ab1ece.js"
+  }
+]);
